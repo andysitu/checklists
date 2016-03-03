35f4bd801e42fcc666fdd3418b32ba88f1b7442f
Andy Situ <and.situ@gmail.com>
2016-03-02 18:04:39 -0800

    Data has internal dataWrapper prop

--- files/data/data.js
+++ files/data/data.js
@@ -1,12 +1,13 @@
 function Data(name, type) {
    this._name = name;
    this._type = type;
+   this._dataWrapper = this._getDataWrapper(type);
    this._data = {};
 }
 
 Data.prototype.make = function(year, month) {
    if (this["_data"][year + "_" + month] === undefined) {
-      var dataObj = this._getDataWrapper(this._type);
+      var dataObj = this._dataWrapper;
       var data = dataObj.makeData(year, month, name);
       this["_data"][year + "_" + month] = data;
    }
