602bb45cc69a3024f0f71463de5061a62963a3ad
Andy Situ <and.situ@gmail.com>
2016-05-10 14:17:45 -0700

    save before restore

--- files/loadup.js
+++ files/loadup.js
@@ -4,19 +4,9 @@ var t1;
 // Run start methods of objs on load.
 function loadup() {
    c = mainCPU = new CPU("main");
-   // cpu.start();
    each(ui, function(funct) {funct();});
 
-
-   // t1 = new Data("test", "click");
-   // t1.addMonth(2016, 2);
-   // var da = t1._getDataArray(2016, 2);
-   // var eleArr = t1.getElements(2016, 2);
-   // console.log("Data Array : ", da);
-   // console.log("Element Array: ", eleArr);
-   // var ele = t1.clicked(2016, 2, 0);
-   // console.log("Clicked Data Array :", da);
-   // console.log("Returned element:", ele);
+   mainCPU.addRow("test", "click");
 }
 
 window.addEventListener("load", loadup);
